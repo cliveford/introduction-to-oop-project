bedf882e1f7fa19c285bb291a7241d59d04eddb9
cliveford <78969930+cliveford@users.noreply.github.com>
2022-03-24 21:22:22 +0000

    feat: Subclass Pigeon
    Added subclass Pigeon which extends base class Bird.  Added eatPizza method to Pigeon.

---
+++ src/main/java/Pigeon.java
@@ -0,0 +1,17 @@
+//The Pigeon extends the base class Bird
+public class Pigeon extends Bird {
+
+    //The Pigeon constructor calls the base class Bird constructor
+    //The name and color parameters are passed to the Bird constructor
+    public Pigeon(String name, String color) {
+        super(name, color);
+    }
+
+    public void eatPizza() {
+        System.out.println("Delicious pizza!");
+    }
+
+    public void sleep() {
+        System.out.println("I'm a sleeping pigeon.");
+    }
+}
