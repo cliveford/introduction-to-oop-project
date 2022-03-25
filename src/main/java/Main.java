62ab05c5211798a2cc0578ff05cf6468f2c374db
cliveford <78969930+cliveford@users.noreply.github.com>
2022-03-25 16:31:46 +0000

    feat: Created Dance interface and modified Main & Bird classes
    Implemented Dance interface in Bird class.
    Modified Main main method so instances invoke dance methods.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,26 @@ public class Main {
         pippa.speak();
         pippa.fly();
         pippa.eatPizza();
+        pippa.spin();
+        pippa.doTheCaterpillar();
+        pippa.jump();
+
 
         Penguin pingu = new Penguin("Pingu", "Black & White");
         pingu.speak();
         pingu.fly();
+        pingu.spin();
+        pingu.doTheCaterpillar();
+        pingu.jump();
+
 
         Parrot polly = new Parrot("Polly", "Red, Yellow & Blue");
         polly.speak();
         polly.fly();
+        polly.spin();
+        polly.doTheCaterpillar();
+        polly.jump();
+
 
     }
 }
